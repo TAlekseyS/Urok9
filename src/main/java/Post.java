48bab5d7d5b8b7a42db3b85fd1eebfd8ef3ca234
Aleksey_T <talekseys@mail.ru>
2023-05-15 14:07:04 +0300

    Sdelal

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+    public String name; // поле для имени
+    public String passport;// добавьте другие поля
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+
+    public FormDate birthday;
+    // заполните другие поля даты рождения
+}
